6706c31af28535212e454faf0fd90e7edf9c5837
Eduarte783 <eric.duarte83@gmail.com>
2022-10-20 13:02:49 -0700

    bjupdate

--- index.js
+++ index.js
@@ -1,23 +1,39 @@
 let firstCard = 10
-let secondCard = 11
+let secondCard = 8
+let cards = [firstCard, secondCard]
 let sum = firstCard + secondCard
 let hasBlackjack = false
 let isAlive = true
+let message = ''
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
 
-if (sum <= 20) {
-  console.log("Do you want another card?")
-  isAlive = false
-} else if (sum === 21) {
-  console.log("Wohoo! You've got Blackjack!")
-  hasBlackjack = true
-} else {
-  console.log("You're out of the game!")
-  isAlive = false
+function startGame() {
+  renderGame()
+}
+function renderGame() {
+  if (sum <= 20) {
+    message = "Do you want another card?"
+    isAlive = false
+  } else if (sum === 21) {
+    message = "Wohoo! You've got Blackjack!"
+    hasBlackjack = true
+  } else {
+    message = "You're out of the game!"
+    isAlive = false
+  }
+  cardsEl.textContent = "Cards: " + cards[0] + " and " + cards[1]
+  sumEl.textContent = "Sum: " + sum
+  messageEl.textContent = message
+
 }
 
-// Cash out 
-console.log(isAlive)
-
+function newCard() {
+  let card = 6
+  sum += card
+  renderGame()
+}
 
 // <<< IF STATEMENTS (PRACTICE 1) >>>>)
 
